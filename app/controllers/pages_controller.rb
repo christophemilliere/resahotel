e633a544a948b4090f79bf9e413de27c3a0495e8
Milliere Christohe <christophemilliere93@gmail.com>
2015-02-26 11:17:31 +0100

    crud users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def home
     @title = "title"
+
+    @user = User.all
+
+    render json:{ user: @user.inspect}
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,40 @@
 class UsersController < ApplicationController
 
-  def create
-		@title = "create"
+	skip_before_filter :verify_authenticity_token, only: [:create,:update, :destroy]
 
-		render json:{test: params.inspect}
+
+  def create
+		@user = User.new
+		@user.name = params[:name]
+		@user.address = params[:address]
+
+		if @user.save
+			render json:{status: 200}
+		else
+			render json:{error: "error"}
+		end
+
   end
 
 	def update
-		@title = "update"
+		@user = User.find_by_id(params[:id])
+		@user.update_attributes(name: params[:name], address: params[:address])
   end
 
 	def show
-		@title = "show"
+		@user = User.find_by_id(params[:id])
+		render json:{ user: @user}
+  end
+	
+	def update
+		@user = User.find_by_id(params[:id])
+		@user.update_attributes(name: params[:name], address: params[:address])
   end
 
-	def delete
-		@title = "delte"
+	def destroy
+		@user = User.find_by_id(params[:id])
+		@user.destroy
+		render json:{ delete: " user delete"}
   end
-
 
 end
